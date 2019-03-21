7bedb09b49c3d0c62dc08f1a05488274fdb48acb
Leonardo Guedez <leolespaul@gmail.com>
2019-03-20 22:00:28 -0400

    Refactor the declaration parser.

--- backend/xml_generator.py
+++ backend/xml_generator.py
@@ -3,6 +3,7 @@ import dicttoxml
 
 def generate(config, filename):
     filename = filename + '.xml'
-    with open(filename, 'w') as f:
-        xmlstr = dicttoxml.dicttoxml(config).decode('utf-8')
-        f.write(xmlstr)
+    with open(filename, 'w') as file:
+        # Generate the XML representation and convert it to UTF-8
+        xmlstr = dicttoxml.dicttoxml(config, attr_type=False).decode('utf-8')
+        file.write(xmlstr)
--- declaration_parser.py
+++ declaration_parser.py
@@ -9,96 +9,107 @@ class DeclarationParser:
     def __init__(self):
         pass
 
-    # TODO: Split into functions for each declaration type
-    def parse_declaration(self, declaration, declarationType):
-        # When variable declaration is found, store it in the symtab
-        if declarationType == 'VariableDeclaration':
-            self.symtab[declaration.name] = declaration.value
+    def parse_variable_declaration(self, declaration):
+        self.symtab[declaration.name] = declaration.value
 
-        if declarationType == 'DetectActivity':
-            for camera in declaration.camera:
-                # If identifier is found, retrieve its entry from the symtab
-                if type(camera).__name__ != 'CameraDeclaration':
+    def parse_detect_activity(self, declaration):
+        for camera in declaration.camera:
+            # If identifier is found, retrieve its entry from the symtab
+            if type(camera).__name__ != 'CameraDeclaration':
 
-                    if camera not in self.symtab:
-                        print('ERROR: UNDEFINED VARIABLE:', camera)
-                        exit(1)
+                if camera not in self.symtab:
+                    print('ERROR: UNDEFINED VARIABLE:', camera)
+                    exit(1)
 
-                    if type(self.symtab[camera]).__name__ != 'CameraDeclaration':
-                        print('ERROR: INCOMPATIBLE TYPE: expecting CameraDeclaration, but found:', type(
-                            self.symtab[camera]).__name__)
-                        exit(1)
-            self.activities.append(declaration.activity_name)
+                if type(self.symtab[camera]).__name__ != 'CameraDeclaration':
+                    print('ERROR: INCOMPATIBLE TYPE: expecting CameraDeclaration, but found:', type(
+                        self.symtab[camera]).__name__)
+                    exit(1)
+        self.activities.append(declaration.activity_name)
 
-        if declarationType == 'TrackObject':
-            if type(declaration.target).__name__ != 'TargetDeclaration':
-                # If not a declaration literal, is an ID
-                target = self.symtab[declaration.target]
+    def parse_track_object(self, declaration):
+        if type(declaration.target).__name__ != 'TargetDeclaration':
+            # If not a declaration literal, is an ID
+            target = self.symtab[declaration.target]
+        else:
+            # Target declaration literal
+            target = declaration.target
+
+        # Get properties of object to track
+        target_properties = {}
+        if target.properties is not None:
+            for prop in target.properties.properties:
+                target_properties[prop.property_name] = prop.property_value
+
+        self.targets[target.target_name] = {}
+        if declaration.min > 0:
+            self.targets[target.target_name]['min'] = declaration.min
+
+        if declaration.max > 0:
+            self.targets[target.target_name]['max'] = declaration.max
+
+        if len(declaration.counter) > 0:
+            self.targets[target.target_name]['counter'] = declaration.counter
+            self.counters.append(declaration.counter)
+
+        if target_properties is not None:
+            self.targets[target.target_name]['properties'] = target_properties
+
+    def parse_when_statement(self, declaration):
+        # Find left and right operands, and operator
+        # Check that the operands are declared variables, or counters
+        if type(declaration.bool_expr.lhoperand).__name__ == 'str':
+            if declaration.bool_expr.lhoperand not in self.counters and declaration.bool_expr.lhoperand not in self.symtab:
+                print('ERROR: UNDECLARED IDENTIFIER:',
+                      declaration.bool_expr.lhoperand)
+                exit(1)
+
+        lhoperand = declaration.bool_expr.lhoperand
+        operator = declaration.bool_expr.operator
+
+        if type(declaration.bool_expr.rhoperand).__name__ == 'str':
+            if declaration.bool_expr.rhoperand not in self.counters and declaration.bool_expr.rhoperand not in self.symtab:
+                print('ERROR: UNDECLARED IDENTIFIER:',
+                      declaration.bool_expr.rhoperand)
+                exit(1)
+
+            if declaration.bool_expr.rhoperand in self.symtab:
+                rhoperand = self.symtab[declaration.bool_expr.rhoperand]
             else:
-                # Target declaration literal
-                target = declaration.target
+                rhoperand = declaration.bool_expr.rhoperand
 
-            # Get properties of object to track
-            target_properties = {}
-            if target.properties is not None:
-                for prop in target.properties.properties:
-                    target_properties[prop.property_name] = prop.property_value
+            if type(rhoperand).__name__ != 'int':
+                print(
+                    'ERROR: Right hand side of when statement must evaluate to integer')
+                exit(1)
 
-            self.targets[target.target_name] = {}
-            if declaration.min > 0:
-                self.targets[target.target_name]['min'] = declaration.min
+        self.conditions.append({
+            'condition': {
+                'left_operand': lhoperand,
+                'operator': operator,
+                'right_operand': rhoperand
+            },
+            'action': declaration.action,
+            'action_args': declaration.arguments
+        })
 
-            if declaration.max > 0:
-                self.targets[target.target_name]['max'] = declaration.max
+    # TODO: Split into functions for each declaration type
+    def parse_declaration(self, declaration, declaration_type):
+        # When variable declaration is found, store it in the symtab
+        if declaration_type == 'VariableDeclaration':
+            self.parse_variable_declaration(declaration)
 
-            if len(declaration.counter) > 0:
-                self.targets[target.target_name]['counter'] = declaration.counter
-                self.counters.append(declaration.counter)
+        if declaration_type == 'DetectActivity':
+            self.parse_detect_activity(declaration)
 
-            if target_properties is not None:
-                self.targets[target.target_name]['properties'] = target_properties
+        if declaration_type == 'TrackObject':
+            self.parse_track_object(declaration)
 
-        if declarationType == 'WhenStatement':
-            # Find left and right operands, and operator
-            # Check that the operands are declared variables, or counters
-            if type(declaration.bool_expr.lhoperand).__name__ == 'str':
-                if declaration.bool_expr.lhoperand not in self.counters and declaration.bool_expr.lhoperand not in self.symtab:
-                    print('ERROR: UNDECLARED IDENTIFIER:',
-                          declaration.bool_expr.lhoperand)
-                    exit(1)
-
-            lhoperand = declaration.bool_expr.lhoperand
-            operator = declaration.bool_expr.operator
-
-            if type(declaration.bool_expr.rhoperand).__name__ == 'str':
-                if declaration.bool_expr.rhoperand not in self.counters and declaration.bool_expr.rhoperand not in self.symtab:
-                    print('ERROR: UNDECLARED IDENTIFIER:',
-                          declaration.bool_expr.rhoperand)
-                    exit(1)
-
-                if declaration.bool_expr.rhoperand in self.symtab:
-                    rhoperand = self.symtab[declaration.bool_expr.rhoperand]
-                else:
-                    rhoperand = declaration.bool_expr.rhoperand
-                
-                if type(rhoperand).__name__ != 'int':
-                    print('ERROR: Right hand side of when statement must evaluate to integer')
-                    exit(1)
-
-
-            self.conditions.append({
-                'condition': {
-                    'left_operand': lhoperand,
-                    'operator': operator,
-                    'right_operand': rhoperand
-                },
-                'action': declaration.action,
-                'action_args': declaration.arguments
-            })
+        if declaration_type == 'WhenStatement':
+            self.parse_when_statement(declaration)
 
     def get_results(self):
         return {
             'targets': self.targets,
             'conditions': self.conditions
         }
-        
